fb2f9fd30b4112370cede723a00926d4abd2c440
Vineet Midha <vineet.midha@yahoo.com>
2020-11-27 22:35:46 +0530

    Create InsertionSortVariant.cpp

---
+++ InsertionSortVariant.cpp
@@ -0,0 +1,23 @@
+
+// https://www.hackerrank.com/challenges/correctness-invariant/problem
+
+void insertionSort(int N, int arr[]) {
+    int i,j;
+    int value;
+    for(i=1;i<N;i++)
+    {
+        value=arr[i];
+        j=i;
+        while(j>0 && value<arr[j-1])
+        {
+            arr[j]=arr[j-1];
+            j=j-1;
+        }
+        arr[j]=value;
+    }
+    for(j=0;j<N;j++)
+    {
+        printf("%d",arr[j]);
+        printf(" ");
+    }
+}
