6e8d726843f3fae7742f26847e4c4f29377aa07e
Vineet Midha <vineet.midha@yahoo.com>
2021-03-11 17:04:16 +0530

    Leonardo's Prime Factors

--- LeonardoPrimeFactors.py
+++ LeonardoPrimeFactors.py
@@ -1,6 +1,6 @@
 # https://www.hackerrank.com/challenges/leonardo-and-prime/problem
 
-primes = [0, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53]
+primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53]
 primorials = [] # product of primes
 
 p = 1
